aef2f4e321b7bddf01f7ddefe3445d13341ab1e0
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-23 02:06:20 +0300

    Part01_Question01

--- OOP04Assignment/Program.cs
+++ OOP04Assignment/Program.cs
@@ -7,9 +7,17 @@
 
             // Part 1
             #region Question01
+            // Question A:
+            // Abstraction is the process of hiding the implementation details
+            // and showing only the essential features of an object to the user
 
-
+            // Question B:
+            // It's important because:
+            // -makes systems easier to maintain and extend
+            // -Hides implementation details
+            // -Reduces implementation details
             #endregion
+
         }
     }
 }
